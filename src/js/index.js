b0f35fcaff7e1eed54592365c1cd58bfa496ce7e
Faouzi <l.a.-noire@hotmail.com>
2019-01-25 11:53:09 +0100

    répartition des noms et des HP par le joueur

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,44 @@ import {Mage} from './partials/_mage.js';
 import {Archer} from './partials/_archer.js';
 import {Boss} from './partials/_boss.js';
 
-let aragorn = new Guerrier('Gimli',100,200,1);
+
+let totalHp = 200;
+
+let choixNom,choixHp,choixAtq;
+
+choixNom = prompt('Choisisez le nom de votre guerrier');
+choixHp = prompt(`Choisisez les HP de ${choixNom} (HP à répartir restants : ${totalHp})`);
+while(choixHp>totalHp){
+    choixHp = prompt(`! Pas assez de HP restants ! Choisisez les HP de ${choixNom} (HP à répartir restants : ${totalHp})`);
+
+}
+totalHp -= choixHp;
+
+let guerrier = new Guerrier(choixNom,choixHp,100,100);
+
+choixNom = prompt('Choisisez le nom de votre mage');
+choixHp = prompt(`Choisisez les HP de ${choixNom} (HP à répartir restants : ${totalHp})`);
+while(choixHp>totalHp){
+    choixHp = prompt(`! Pas assez de HP restants ! Choisisez les HP de ${choixNom} (HP à répartir restants : ${totalHp})`);
+
+}
+totalHp -= choixHp;
 
 
+let mage = new Mage(choixNom,choixHp,100,100);
 
-console.log(aragorn);
+choixNom = prompt('Choisisez le nom de votre archer');
+choixHp = prompt(`Choisisez les HP de ${choixNom} (HP à répartir restants : ${totalHp})`);
+while(choixHp>totalHp){
+    choixHp = prompt(`! Pas assez de HP restants ! Choisisez les HP de ${choixNom} (HP à répartir restants : ${totalHp})`);
+
+}
+totalHp -= choixHp;
+
+
+let archer = new Archer(choixNom,choixHp,100,100);
+
+
+console.log(guerrier);
+console.log(mage);
+console.log(archer);
